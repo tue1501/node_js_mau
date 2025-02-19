b3b168785b51e03e77abbac023f30ce85528a162
tue1501 <minhtue22112004@gmail.com>
2025-02-19 13:55:28 +0700

    frist

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,11 @@ import mysql from 'mysql2';
 
 // Tạo kết nối cơ sở dữ liệu MySQL
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',  // Thay đổi thành thông tin của bạn
-    password: '',  // Thay đổi mật khẩu nếu có
-    database: 'petland'  // Thay đổi tên cơ sở dữ liệu
+    host: 'mysql-121bae9e-tuedeptrai1501-28a4.h.aivencloud.com',
+    port: '23358',
+    user: 'avnadmin',  // Thay đổi thành thông tin của bạn
+    password: 'AVNS_9FCh7TMB3FGxqIgMOM2',  // Thay đổi mật khẩu nếu có
+    database: 'defaultdb'  // Thay đổi tên cơ sở dữ liệu
 });
 
 export default connection.promise();  // Sử dụng promise để tương thích với async/await
