0893f1b97866ce971e1695b9bf76fbe0d6cdaaab
tue1501 <minhtue22112004@gmail.com>
2025-02-27 00:40:05 +0700

    productcontroller create blacklist

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 // Cấu hình Twilio
 const accountSid = 'ACc8feb03439456d07ac6446842c3c8d7a';
-const authToken = '0ecaa3f044999686f19cb806584d5a05';
+const authToken = '5eb5bf21f457ba3d8eeaedd23ce4bd8c';
 
 // const accountSid = process.env.TWILIO_ACCOUNT_SID;
 // const authToken = process.env.TWILIO_AUTH_TOKEN;
