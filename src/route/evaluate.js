37acae75044e0b166df19aa799a8fb544faf0b22
tue1501 <minhtue22112004@gmail.com>
2025-04-26 23:52:06 +0700

    update

--- src/route/evaluate.js
+++ src/route/evaluate.js
@@ -7,7 +7,7 @@ const Evaluate = (app) => {
     router.get("/evaluate/:idSanPham",authenticateJWT, EvaluateController.getEvaluate);
     router.get("/evaluateByIdMau/:idMau",authenticateJWT, EvaluateController.getEvaluateByIdMau);
     router.post("/evaluate",authenticateJWT, EvaluateController.addEvaluate);
-    router.get("/evaluate",authenticateJWT, EvaluateController.getallEvaluate);
+    router.get("/evaluate", EvaluateController.getallEvaluate);
     router.get("/detailevaluate/:id",authenticateJWT, EvaluateController.detailevaluate);
     return app.use('/api', router);
 }
